9bf4dca7990fb5ae32bc29954ef8b53cca0b01cc
Alex Winter <alex@bitcoinvn.io>
2021-12-09 16:59:17 -0300

    Fix datetime in deposit model
    Took 12 minutes

--- src/Model/Order/Deposit.php
+++ src/Model/Order/Deposit.php
@@ -14,6 +14,7 @@ class Deposit
     private float $settleAmount;
     private ?float $networkFeeAmount = null;
     /**
+     * @Serializer\SerializedName("createdAtISO")
      * @Serializer\Type("DateTime<'Y-m-d\TH:i:s.vP'>")
      */
     private ?\DateTime $createdAt = null;
